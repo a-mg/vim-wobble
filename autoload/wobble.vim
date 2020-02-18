a-mg/vim-wobble

commit 1b67ffeb99ea170e8637e825b0a115267d5c93c1

Remove legacy `xml_prefix` setting

autoload/wobble.vim
@@ -1,7 +1,3 @@
-function! s:PrefixMapping(map)
-  return substitute(a:map, '\~', g:wobble_xml_prefix, '')
-endfunction
-
 function! wobble#MapTextObject(sequence, name, map, norm, exe)
   let plugstring =
         \  'noremap <silent><buffer> '

plugin/wobble.vim
@@ -19,4 +19,3 @@ endfunction
 call s:PluginSetting('map_textobjects' , 1)
 call s:PluginSetting('map_localleader' , 1)
 call s:PluginSetting('add_keywords'    , 1)
-call s:PluginSetting('xml_prefix'      , '"h"')

commit 86bebafcde2b8ae3a3e57cf4dc4eeea8e7a4839c

Fix bug introduced by 1b67ffe

autoload/wobble.vim
@@ -15,7 +15,7 @@ function! wobble#MapTextObject(sequence, name, map, norm, exe)
   if g:wobble_map_textobjects
     let mapstring =
           \  'map <silent><buffer> '
-          \. s:PrefixMapping(a:map)
+          \. a:map
           \. ' <Plug>Wobble_' . a:name
 
     execute 'o' . mapstring
@@ -34,7 +34,7 @@ function! wobble#MapLocalLeader(sequence, name, map)
   if g:wobble_map_localleader
     let mapstring =
           \  'map <silent><buffer> '
-          \. '<localleader>' . s:PrefixMapping(a:map)
+          \. '<localleader>' . a:map
           \. ' <Plug>Wobble_' . a:name
 
     execute 'n' . mapstring