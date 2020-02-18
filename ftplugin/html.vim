a-mg/vim-wobble

commit ce47dfa265b87ffc57a884cc922b004b4f178ea7

Fix iName and iValue in multi-line attr lists

ftplugin/html.vim
@@ -5,8 +5,8 @@ let b:loaded_wobble_html = 1
 
 
 
-call wobble#MapTextObject('f"F=F v2f"'                   , 'aAttr'     , 'aA')
-call wobble#MapTextObject('f"F=T vt='                    , 'iName'     , 'iN')
+call wobble#MapTextObject('f"F=Bv2f"'                    , 'aAttr'     , 'aA')
+call wobble#MapTextObject('f"F=Bvt='                     , 'iName'     , 'iN')
 call wobble#MapTextObject('f"F=f"lvi"'                   , 'iValue'    , 'iV')
 
 call wobble#MapTextObject('vato\ef vt>'                  , 'aTagAttrs' , 'aL' , '+e')

commit 66fbceaf9ccbee11d5f4afb5edb2002c8cf2152b

Fix iName in cases with whitespace
New mapping in ce47dfa introduced a bug that would select whitespace if it existed between the name and the =. This trims whitespace from the selection if it exists

ftplugin/html.vim
@@ -6,7 +6,7 @@ let b:loaded_wobble_html = 1
 
 
 call wobble#MapTextObject('f"F=Bv2f"'                    , 'aAttr'     , 'aA')
-call wobble#MapTextObject('f"F=Bvt='                     , 'iName'     , 'iN')
+call wobble#MapTextObject('f"F=gevBo'                    , 'iName'     , 'iN')
 call wobble#MapTextObject('f"F=f"lvi"'                   , 'iValue'    , 'iV')
 
 call wobble#MapTextObject('vato\evi<of o'                , 'aTagAttrs' , 'aL' , '+e')